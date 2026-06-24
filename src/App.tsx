dd8bc945aeebd7a67cbefe983b2decd6a5ed27b4
Gitterest <mcshootem@proton.me>
2026-06-24 06:58:36 -0700

    Build phone flip calculator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { AppHeader } from './components/AppHeader'
 import { HomePage } from './pages/HomePage'
 import { NotFoundPage } from './pages/NotFoundPage'
+import { PhoneFlipPage } from './pages/PhoneFlipPage'
 import { PowerToolFlipPage } from './pages/PowerToolFlipPage'
 
 export function App() {
@@ -11,6 +12,7 @@ export function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/calculators/phone-flip" element={<PhoneFlipPage />} />
           <Route path="/calculators/power-tool-flip" element={<PowerToolFlipPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
